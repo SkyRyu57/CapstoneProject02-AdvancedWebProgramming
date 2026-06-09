9690dcfa8109d41849cfbe674999355501997f04
SkyRyu57 <liuricko57@gmail.com>
2026-06-09 13:07:56 +0700

    Fix duplicate Daftar Barang navigation link

--- frontend-laravel/resources/views/components/app-nav.blade.php
+++ frontend-laravel/resources/views/components/app-nav.blade.php
@@ -24,8 +24,8 @@
                 <a href="{{ route('admin.users.index') }}" @class(['active' => request()->routeIs('admin.users.*')])>Pengguna</a>
                 <a href="{{ route('admin.rooms.index') }}" @class(['active' => request()->routeIs('admin.rooms.*')])>Ruangan</a>
             @else
-                <a href="{{ route('inventory-list.index') }}" @class(['active' => request()->routeIs('inventory-list.index')])>Daftar Barang</a>
                 
+
                 @if ($role === 'kepala_lab')
                     <a href="{{ route('kepala-lab.drafts.index') }}" @class(['active' => request()->routeIs('kepala-lab.drafts.*')])>Draf Pengadaan</a>
                 @elseif ($role === 'kaprodi')
